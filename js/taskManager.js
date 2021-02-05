8b488b400bdce46b5cd4570a5efa465bf6211b35
Rita Frias <rfrias008@gmail.com>
2021-02-05 09:13:10 -0600

    defines object within task manager task

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,15 +1,27 @@
 class TaskManager {
   constructor(currentId = 0) {
-    this.tasks = [];
-    this.currentId = currentId;
+      this.tasks = [];
+      this.currentId = currentId;
   }
-
+    addTask(jobName, desc, assign, due, stat) {
+      this.currentId ++
+      const taskName = {
+        name: jobName,
+        description: desc,
+        aissgnTo: assign,
+        dueDate: due,
+        status: stat
+      }
+      this.tasks.push(taskName);
+    }
   
-  addTask(name, description, assignedTo, dueDate, status = 'TODO') {
-    this.currentId++;
-    this.tasks.push(
-      { id: this.currentId, name: name, description: description, assignedTo: assignedTo, dueDate: dueDate, status: status }
-    );
-  };
-
 }
+
+const taskArray = new TaskManager();
+
+taskArray.addTask('get groceries', 'des', 'due', 'done');
+taskArray.addTask('ballet', 'des', 'due', 'to-do');
+taskArray.addTask('soccer practice', 'des', 'due', 'in-process');
+
+console.log(taskArray);
+
